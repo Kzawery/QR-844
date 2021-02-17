e870f428da80c49abf68207cba4c0aee82e8ac14
Kzawery <46087149+Kzawery@users.noreply.github.com>
2021-02-17 11:06:42 +0100

    first commit

---
+++ app/src/main/java/com/example/qr/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.qr;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.WriterException;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView qrCodeIV;
+    private EditText dataEdt;
+    private Button generateQrBtn;
+    private Button cameraBtn;
+    Bitmap bitmap;
+    QRGEncoder qrgEncoder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initializing all variables.
+        qrCodeIV = findViewById(R.id.idIVQrcode);
+        dataEdt = findViewById(R.id.idEdt);
+        generateQrBtn = findViewById(R.id.idBtnGenerateQR);
+        cameraBtn = findViewById(R.id.idBtnCamera);
+
+        // initializing onclick listener for button.
+        generateQrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(dataEdt.getText().toString())) {
+
+                    // if the edittext inputs are empty then execute
+                    // this method showing a toast message.
+                    Toast.makeText(MainActivity.this, "Enter some text to generate QR Code", Toast.LENGTH_SHORT).show();
+                } else {
+                    // below line is for getting
+                    // the windowmanager service.
+                    WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+                    // initializing a variable for default display.
+                    Display display = manager.getDefaultDisplay();
+
+                    // creating a variable for point which
+                    // is to be displayed in QR Code.
+                    Point point = new Point();
+                    display.getSize(point);
+
+                    // getting width and
+                    // height of a point
+                    int width = point.x;
+                    int height = point.y;
+
+                    // generating dimension from width and height.
+                    int dimen = width < height ? width : height;
+                    dimen = dimen * 3 / 4;
+
+                    // setting this dimensions inside our qr code
+                    // encoder to generate our qr code.
+                    qrgEncoder = new QRGEncoder(dataEdt.getText().toString(), null, QRGContents.Type.TEXT, dimen);
+                    // getting our qrcode in the form of bitmap.
+                    bitmap = qrgEncoder.getBitmap();
+                    // the bitmap is set inside our image
+                    // view using .setimagebitmap method.
+                    qrCodeIV.setImageBitmap(bitmap);
+                }
+            }
+        });
+
+        cameraBtn.setOnClickListener(v -> {
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CAMERA},
+                    1);
+
+            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+            startActivity(intent);
+
+
+
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+}
